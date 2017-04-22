cdb1ce6d0fadf1be46a809ba4498825fd7c67699
hjl4347570 <hjl4347570@gmail.com>
2017-04-23 00:54:47 +0800

    add

--- www/views/product/product-add.controller.js
+++ www/views/product/product-add.controller.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   angular.module('prodcut-add.controllers', [])
-    .controller('ProductAddCtrl', ['$scope', '$state', '$ionicPopup', 'ProductService', 'CategoryService', '$ionicHistory', '$stateParams', '$cordovaBarcodeScanner', '$cordovaCamera', function ($scope, $state, $ionicPopup, ProductService, CategoryService, $ionicHistory, $stateParams, $cordovaBarcodeScanner, $cordovaCamera) {
+    .controller('ProductAddCtrl', ['$scope', '$state', '$ionicPopup', '$ionicActionSheet', 'ProductService', 'CategoryService', '$ionicHistory', '$stateParams', '$cordovaBarcodeScanner', '$cordovaCamera', function ($scope, $state, $ionicPopup, $ionicActionSheet, ProductService, CategoryService, $ionicHistory, $stateParams, $cordovaBarcodeScanner, $cordovaCamera) {
       $scope.product = {
         images: [],
         barcode: '',
@@ -27,9 +27,30 @@
       $scope.scanBarcode = function () {
         $cordovaBarcodeScanner.scan().then(function (barcodeData) {
           $scope.product.barcode = barcodeData.text;
-           alert(barcodeData.text);
-        },function (error) {
+          alert(barcodeData.text);
+        }, function (error) {
           alert(error);
+        })
+      }
+      function camera() {
+        var options = {
+          quality: 50,
+        }
+      }
+      $scope.showActionSheet = function () {
+        $ionicActionSheet.show({
+          buttons: [{ 'text': '拍照' }, { 'text': '从相册中选取' }],
+          cancelText: '<b>取消</b>',
+          buttonClicked: function (index) {
+            switch (index) {
+              case 0:
+                camera();
+                break;
+              case 1:
+                pickImage();
+                break;
+            }
+          }
         })
       }
       $scope.save = function () {
